19565a99b308a93f9ca04f05dbc5efb32b31f728
Filippo Valle <filippo_valle@protonmail.com>
2024-03-13 22:07:07 +0100

    add fun fact

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,12 +1,40 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:fussball/stats.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
 
 class GamePage extends StatelessWidget {
   final Partita game;
   static DateFormat format = DateFormat("dd-MM-yyyy");
+  Future<String>? _randomfact;
 
-  const GamePage({super.key, required this.game});
+  Future<String> _getRandomEvent(DateTime date) async {
+    var response = await http.get(Uri.parse(
+        "https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/${date.month}/${date.day}"),
+        headers: {"User-Agent":"online.fvalle.fussball"}
+        );
+
+    if (response.statusCode == 200) {
+      var events = jsonDecode(response.body)["events"];
+      Random random = Random();
+
+      int randomIndex = random.nextInt(events.length);
+
+      // Get the random element from the array
+      var randomElement = events[randomIndex];
+
+      return "On this day in ${randomElement["year"]} ${randomElement["text"]}";
+    } else {
+      throw Exception(response.body);
+    }
+  }
+
+  GamePage({super.key, required this.game}) {
+    _randomfact = _getRandomEvent(game.data ?? DateTime.now());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +72,7 @@ class GamePage extends StatelessWidget {
                   const Spacer()
                 ],
               ),
+              const Spacer(),
               Row(
                 children: Iterable<Icon>.generate(
                     game.score[0],
@@ -55,7 +84,22 @@ class GamePage extends StatelessWidget {
                     game.score[1],
                     (i) => const Icon(Icons.sports_soccer_outlined,
                         color: Colors.blue)).toList(),
-              )
+              ),
+              const Spacer(),
+              FutureBuilder(
+                  future: _randomfact,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!);
+                    } else if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    } else {
+                      return Container();
+                    }
+                  }),
+              const Spacer(
+                flex: 5,
+              ),
             ],
           ),
         )));
